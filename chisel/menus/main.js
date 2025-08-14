0ee096570e3963ba7e30b3c661131ed637a8e7e8
John Rigler <john@rigler.org>
2025-08-14 14:26:46 -0500

    Add files via upload

--- chisel/menus/main.js
+++ chisel/menus/main.js
@@ -4,11 +4,12 @@ mainMenu.tools.add("welcome","Welcome")
 mainMenu.tools.add("demo","Demonstration")
 mainMenu.tools.add("address","Ledger View")
 mainMenu.tools.add("import","Import Image")
-mainMenu.tools.add("draw","Draw")
+mainMenu.tools.add("tablet","Create Tablet")
 mainMenu.tools.add("wallet","Wallet")
-mainMenu.tools.add("save", "Save/Load");
 mainMenu.tools.add("help", "Help");
 mainMenu.tools.add("about","About");
+mainMenu.tools.add("whitepaper","Whitepaper");
+
 
 mainMenu.tools.addSubmenu("help","about", "About", "");
 mainMenu.tools.addSubmenu("help","demo", "Demo", "");
