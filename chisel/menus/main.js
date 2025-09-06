b6e9c6e71267b5f1da448e658de60ead406d791e
John Rigler <john@rigler.org>
2025-09-06 00:34:46 -0500

    .

--- chisel/menus/main.js
+++ chisel/menus/main.js
@@ -6,11 +6,12 @@ mainMenu.tools.add("address","Ledger View")
 mainMenu.tools.add("import","Import Image")
 mainMenu.tools.add("tablet","Create Tablet")
 mainMenu.tools.add("wallet","Wallet")
-mainMenu.tools.add("help", "Help");
+mainMenu.tools.add("eval", "Eval");
 mainMenu.tools.add("about","About");
 mainMenu.tools.add("whitepaper","Whitepaper");
 
-
+/*
 mainMenu.tools.addSubmenu("help","about", "About", "");
 mainMenu.tools.addSubmenu("help","demo", "Demo", "");
 mainMenu.tools.addSubmenu("help","config", "Config", "");
+*/
